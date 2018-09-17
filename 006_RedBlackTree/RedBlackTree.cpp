9f1abd65933eb55c650eec8cd6e179d7c412bd7a
kthss01 <kthss01@naver.com>
2018-09-17 22:29:44 +0900

    6일차 RedBlackTree

--- 006_RedBlackTree/RedBlackTree.cpp
+++ 006_RedBlackTree/RedBlackTree.cpp
@@ -1,70 +1,277 @@
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
-// enum class class 내에 소속된거라 전역이 아님
-enum class Color
+class RedBlackTree
 {
-	Red = 0, Black
-};
+private:
+	struct Node;
+	// enum class class 내에 소속된거라 전역이 아님
+	enum class Colors { Red = 0, Black };
 
-struct Node
-{
-	Node* Parent;
-	Node* Left, * Right;
-
-	Color Color;
-	int Data;
-};
-
-Node* Nil = new Node();
-
-// parent 삽입될 노드의 부모
-void RotationRight(Node** root, Node* parent)
-{
-	// 왼쪽 자식의 오른쪽 자식노드 -> 부모 노드의 오른쪽 자식
-	Node* leftChild = parent->Left;
-	parent->Right = leftChild->Right;
-
-
-	if (leftChild->Right != Nil)
-		leftChild->Right->Parent = parent;
-
-	leftChild->Parent = parent;
-
-	
-	if (parent->Parent == NULL) // 부모가 루트라면 왼쪽 자식을 루트로 -> 우회전 준비
+public:
+	RedBlackTree(int data)
 	{
-		*root = leftChild;
+		root = new Node(data);
+		root->Color = Colors::Black;
+		root->L = nil;
+		root->R = nil;
 	}
-	else
+
+	~RedBlackTree()
 	{
-		if (parent == parent->Parent->Left) // 왼쪽 자식 노드를 부모가 있던 곳으로 올림
-			parent->Parent->Left = leftChild;
+
+	}
+
+	void Insert(int data)
+	{
+		Node* node = new Node(data);
+		Insert(root, node);
+
+		node->Color = Colors::Red; // 처음 넣을때는 일단 Red Rebuild일 때 검증
+		node->L = nil;
+		node->R = nil;
+
+		Rebuild(node);
+	}
+
+private:
+	void Insert(Node* node, Node* newNode)
+	{
+		assert(node->Data != newNode->Data);
+
+		if (node->Data < newNode->Data)
+		{
+			if (node->R == nil)
+			{
+				node->R = newNode;
+				newNode->P = node;
+			}
+			else
+				Insert(node->R, newNode);
+		}
 		else
-			parent->Parent->Right = leftChild;
+		{
+			if (node->L == nil)
+			{
+				node->L = newNode;
+				newNode->P = node;
+			}
+			else
+				Insert(node->L, newNode);
+		}
 	}
 
-	leftChild->Right = parent;
-	parent->Parent = leftChild;
-}
+	void RotateRight(Node* parent) 
+	{
+		Node* leftChild = parent->L;
+		parent->L = leftChild->R;
 
-void Insert(Node** tree, Node* node)
+		if (leftChild->R != nil)
+			leftChild->R->P = parent;
+
+		leftChild->P = parent->P;
+
+		if (parent->P == NULL)
+		{
+			root = leftChild;
+		}
+		else
+		{
+			if (parent == parent->P->L)
+				parent->P->L = leftChild;
+			else
+				parent->P->R = leftChild;
+		}
+
+		leftChild->R = parent;
+		parent->P = leftChild;
+	}
+
+	void RotateLeft(Node* parent)
+	{
+		Node* rightChild = parent->R;
+		parent->R = rightChild->L;
+
+		if (rightChild->L != nil)
+			rightChild->L->P = parent;
+
+		rightChild->P = parent->P;
+
+		if (parent->P == NULL)
+		{
+			root = rightChild;
+		}
+		else
+		{
+			if (parent == parent->P->L)
+				parent->P->L = rightChild;
+			else
+				parent->P->R = rightChild;
+		}
+
+		rightChild->L = parent;
+		parent->P = rightChild;
+	}
+
+	void Rebuild(Node* node)
+	{
+		while (node != root && node->P->Color == Colors::Red)
+		{
+			if (node->P == node->P->P->L)
+			{
+				Node* uncle = node->P->P->R;
+				// uncle이 Red일때는 Recoloring 
+				if (uncle->Color == Colors::Red)
+				{
+					node->P->Color = Colors::Black;
+					uncle->Color = Colors::Black;
+					node->P->P->Color = Colors::Red;
+
+					node = node->P->P;
+				}
+				// uncle이 Black일때는 Restructuring
+				else
+				{
+					if (node == node->P->R)
+					{
+						node = node->P;
+
+						RotateLeft(node);
+					}
+
+					node->P->Color = Colors::Black;
+					node->P->P->Color = Colors::Red;
+
+					RotateRight(node->P->P);
+				}
+			} // if(node->P)
+			else 
+			{
+				Node* uncle = node->P->P->L;
+				if (uncle->Color == Colors::Red)
+				{
+					node->P->Color = Colors::Black;
+					uncle->Color = Colors::Black;
+					node->P->P->Color = Colors::Red;
+
+					node = node->P->P;
+				}
+				else
+				{
+					if (node == node->P->L)
+					{
+						node = node->P;
+
+						RotateRight(node);
+					}
+
+					node->P->Color = Colors::Black;
+					node->P->P->Color = Colors::Red;
+
+					RotateLeft(node->P->P);
+				}
+			}
+
+		} // while(node)
+
+		root->Color = Colors::Black;
+	}
+
+public:
+	void Print()
+	{
+		Print(root, 0, 0);
+	}
+
+private:
+	void Print(Node* node, int depth, int blackCount)
+	{
+		int i = 0;
+		char c = 'X'; // child인지 아닌지
+		int v = 0; // 부모의 data
+
+		if (node == NULL || node == nil)
+			return;
+
+		if (node->Color == Colors::Black)
+			blackCount++;
+
+		if (node->P != NULL)
+		{
+			v = node->P->Data;
+
+			if (node->P->L == node)
+				c = 'L';
+			else
+				c = 'R';
+		}
+
+		for (i = 0; i < depth; i++)
+			cout << " ";
+
+		cout << node->Data << "-" << c << " ";
+		cout << ((node->Color == Colors::Red) ? "Red" : "Black");
+		cout << " [" << v << "] " << "//" << depth << endl;
+
+		//if (node->L == nil && node->R == nil)
+		//	cout << "-------------- " << blackCount << endl;
+
+		Print(node->L, depth + 1, blackCount);
+		Print(node->R, depth + 1, blackCount);
+	}
+
+private:
+	Node* nil = new Node();
+
+	struct Node
+	{
+		int Data;
+
+		Node* P; // parent
+		Node* L; // left
+		Node* R; // right
+
+		Colors Color;
+
+		Node()
+		{
+			P = L = R = NULL;
+			memset(&Data, 0, sizeof(int));
+
+			Color = Colors::Black;
+		}
+
+		Node(int data) : P(NULL)
+		{
+			Data = data;
+		}
+	};
+
+	Node* root;
+};
+
+void Print(int end)
 {
-	InsertNode(tree, node); // 노드 삽입
 
-	node->Color = Color::Red;
-	node->Left = Nil;
-	node->Right = Nil;
+	RedBlackTree* rbt = new RedBlackTree(1);
+	for (int i = 2; i <= end; i++)
+		rbt->Insert(i);
 
-	// 이때가 검증절차
-	Rebuild(tree, node);
+	cout << "-------" << end << "-------" << endl;
+
+	rbt->Print();
+
+	cout << endl;
+
+	delete rbt;
 }
 
 void main()
 {
-	Nil->Left = Nil->Right = Nil->Parent = NULL;
-	Nil->Color = Color::Black;
-	Nil->Data = 0;
-
-
+	Print(2);
+	Print(3);
+	Print(4);
+	Print(5);
+	Print(6);
 }
